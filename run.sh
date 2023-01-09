ab09180b6c94098bac9345703fba75a351cc94af
Yogurt <yogurtyy@yahoo.com>
2023-01-09 09:28:04 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+su
+cat universe >> /etc/pacman.conf
+pacman -Sy
+pacman-key --refresh
+pacman -Sy artix-linux-support
+cat repos >> /etc/pacman.conf
+pacman-key --populate archlinux
+pacman -Syu
+
+pacman -S git base-devel chromium go lua nvidia nvidia-utils virtualbox code pypy3 nodejs npm ffmpeg git wget micro fish zsh youtube-dl noto-fonts-emoji gucharmap pulseaudio pavucontrol bluez ranger ncdu flameshot
