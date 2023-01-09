e0b8bdb53bdfff6b592f010715a3b582e5ffd730
Yogurt <yogurtyy@yahoo.com>
2023-01-09 09:47:10 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
+if (whoami != root)
+  then echo "You have to run as root (sudo)."
+  exit 1
+fi
 cat repos >> /etc/pacman.conf
 cat universe >> /etc/pacman.conf
 pacman -Sy
 pacman-key --refresh
 pacman-key --init
 pacman -Sy artix-archlinux-support
-
 pacman-key --populate archlinux
 pacman -Syu
-
+# personal selection:
 pacman -S git base-devel chromium go lua nvidia nvidia-utils virtualbox code pypy3 nodejs npm ffmpeg git wget micro fish zsh youtube-dl noto-fonts-emoji gucharmap pulseaudio pavucontrol bluez ranger ncdu flameshot
