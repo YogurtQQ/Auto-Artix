201571cc101267251511541f05142ec2d8c00a74
Yogurt <yogurtyy@yahoo.com>
2023-01-09 10:06:28 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,3 +13,5 @@ pacman-key --populate archlinux
 pacman -Syu
 # personal selection:
 pacman -S git base-devel chromium go lua nvidia nvidia-utils virtualbox code pypy3 nodejs npm ffmpeg git wget micro fish zsh youtube-dl noto-fonts-emoji gucharmap pulseaudio pavucontrol bluez ranger ncdu flameshot
+# (my cinnamon key shortcuts)
+dconf load /org/cinnamon/desktop/keybindings/ < dconf-settings.conf
