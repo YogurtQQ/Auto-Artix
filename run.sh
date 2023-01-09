6a6d2f804ff9e5b8fec31bda1d3dde75e83af9b2
Yogurt <yogurtyy@yahoo.com>
2023-01-09 11:56:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ pacman-key --refresh
 yes | pacman -Sy artix-archlinux-support
 cat mirrorlist.conf >> /etc/pacman.conf
 pacman-key --populate archlinux
-pacman -Syu
+yes | pacman -Syu
 # personal selection:
-pacman -S git base-devel chromium go lua nvidia nvidia-utils virtualbox code pypy3 nodejs npm ffmpeg git wget micro fish zsh youtube-dl noto-fonts-emoji gucharmap pulseaudio pavucontrol bluez ranger ncdu flameshot
+yes | pacman -S git base-devel chromium go lua nvidia nvidia-utils virtualbox code pypy3 nodejs npm ffmpeg git wget micro fish zsh youtube-dl noto-fonts-emoji gucharmap pulseaudio pavucontrol bluez ranger ncdu flameshot
 # (my cinnamon key shortcuts)
 dconf load /org/cinnamon/desktop/keybindings/ < dconf-settings.conf
